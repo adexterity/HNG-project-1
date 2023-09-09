f0cfc83c895ba0fccbbcf3d770b5da00e7873361
joseph adeiza <opensesame4me@gmail.com>
2023-09-09 17:33:29 +0100

    updated

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
  // selecting HTML elements
-//  const userName = document.querySelector('[data-testid="slackUserName"]');
-//  const profilePics = document.querySelector('[data-testid="slackDisplayImage"]');
- const day = document.querySelector('[data-testid="currentDayofTheWeek"]');
- const time = document.querySelector('[data-testid="currentUTCTime"]');
-//  const track = document.querySelector('[data-testid="myTrack"]');
-//  const github = document.querySelector('[data-testid="githubURL"]');
- 
- // username
-//  userName.textContent='Adexterity';
 
- // profile pics
-//  profilePics.setAttribute('src', './joseph.jpg')
+ const day = document.querySelector('.day');
+ const time = document.querySelector('.time');
+
  
  // day of the week 
  const date = new Date();
@@ -23,17 +15,11 @@
 
  // displaying the time
  function updateTime() {
-     const currentTime = Date.now();
-     time.textContent = currentTime;
+     const date = new Date();
+     time.textContent = date.getTime();
  }
 
  updateTime();
  //update the clock
  setInterval(updateTime, 100);
 
- // track
- track.textContent= 'Frontend';
-
- // github profile
-//  github.setAttribute('href', 'https://github.com/adexterity/HNG-project-1')
-//  github.textContent = 'Github Profile';
